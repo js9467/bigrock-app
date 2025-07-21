c896baa36d31de3d28deb36b4441e99140e6110f
js9467 <64589684+js9467@users.noreply.github.com>
2025-07-21 18:20:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -584,8 +584,7 @@ def index():
 
     theme_class = f"theme-{tournament.lower().replace(' ', '-')}"
     version = get_version()
-
-  return render_template("index.html", theme_class=theme_class, version=version, settings=settings)
+    return render_template("index.html", theme_class=theme_class, version=version, settings=settings)
 
 
     

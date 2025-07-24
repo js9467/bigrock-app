412897f0dfd5a22d91fe6604e334a7aaffcaf25a
js9467 <64589684+js9467@users.noreply.github.com>
2025-07-23 22:52:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -604,7 +604,8 @@ from flask import render_template_string
 PARTICIPANTS_CACHES = {}  # tournament_key: {"last_time": 0, "data": []}
 
 
-except Exception as e:
+
+    except Exception as e:
         print(f"❌ Playwright error for {tournament}: {e}")
         print(f"URL: {url}")
         print(f"Config: {config}")

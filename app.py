7d5d88cde34a39761bd2cba73f83fcf49a6292f1
js9467 <64589684+js9467@users.noreply.github.com>
2025-08-03 23:09:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ ALERTS_FILE = 'alerts.json'
 NOTIFIED_FILE = 'notified.json'
 
 SMTP_USER = "bigrockapp@gmail.com"        # use a Gmail account
-SMTP_PASS = "zxdsxfgbabgdkrxt"          # must be a Gmail app password
+SMTP_PASS = "uxgjfdeajsyqlosw"          # must be a Gmail app password
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 
